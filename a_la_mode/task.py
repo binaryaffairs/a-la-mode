c63486997fac444e70eb6651ffc07251a8fdda30
thattommyhall <thattommyhall@gmail.com>
2020-02-07 16:37:03 +0000

    dont use `first`

--- a_la_mode/task.py
+++ a_la_mode/task.py
@@ -39,7 +39,7 @@ def encode(task):
         result = assoc(task.spec, 'inputs', task.inputs)
         output_sha = sha1(bencode(result))
         return assoc(result, 'output', output_sha)
-    first_dep = first(task.deps)
+    first_dep = task.deps[0]
     inputs = assoc(task.inputs, first_dep.name, encode(first_dep)['output'])
     return encode(Task(task.name, task.spec, task.deps[1:], inputs))
 
