34ecbc24a976df5ceef6edda999b2c0532518d3b
thattommyhall <thattommyhall@gmail.com>
2020-02-07 18:50:22 +0000

    Test changing one of the Tasks

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -1,5 +1,6 @@
 from a_la_mode import Task, Dag, sha, dissoc
 from bencode import bencode
+from toolz.dicttoolz import dissoc
 import pytest
 
 eg_dag = Dag({
@@ -42,6 +43,32 @@ for task in [eg_dag.blur, eg_dag.edge_enhance]:
     eg_dag.collage.add_dep(task)
 
 encoded_dag = eg_dag.encode()
+eg_dag.blur.spec['sha'] = 'qwetr3'
+
+changed_dag = eg_dag
+encoded_changed_dag = changed_dag.encode()
+
+def test_unchanged():
+    assert encoded_dag['meta'] == encoded_changed_dag['meta']
+    for task in ['download_images', 'edge_enhance']:
+        assert encoded_dag['tasks'][task] == encoded_changed_dag['tasks'][task]
+
+def test_changed():
+    # blurs sha and output changed
+    assert encoded_dag['tasks']['blur']['sha'] != encoded_changed_dag['tasks']['blur']['sha']
+    assert encoded_dag['tasks']['blur']['output'] != encoded_changed_dag['tasks']['blur']['output']
+
+    # only blurs sha and output changed
+    assert dissoc(encoded_dag['tasks']['blur'], 'sha', 'output') == \
+           dissoc(encoded_changed_dag['tasks']['blur'], 'sha', 'output')
+
+    # collages blur input changed
+    assert encoded_dag['tasks']['collage']['inputs']['blur'] != \
+           encoded_changed_dag['tasks']['collage']['inputs']['blur']
+
+    # only collages blur input changed
+    assert dissoc(encoded_dag['tasks']['collage']['inputs'], 'blur') == \
+           dissoc(encoded_changed_dag['tasks']['collage']['inputs'], 'blur')
 
 def test_outputs():
     for _name, task in encoded_dag['tasks'].items():
